fc124d6ae1de1705f8771b4d202d834238f387fa
roryclear <roryclear.rc@gmail.com>
2026-06-19 21:48:33 +0100

    just copy in file

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-from omnivoice import OmniVoice
+from omni import OmniVoice
 import soundfile as sf
 import torch
-from typing import Union, Optional
 from dataclasses import dataclass, fields
 
 @dataclass
@@ -24,82 +23,6 @@ class OmniVoiceGenerationConfig:
         filtered = {k: v for k, v in kwargs_dict.items() if k in valid_keys}
         return cls(**filtered)
 
-# https://github.com/k2-fsa/OmniVoice/blob/9948396864cb713b0c2f92495cf4449bd8717127/omnivoice/models/omnivoice.py#L475
-@torch.inference_mode()
-def generate(
-    model,
-    text: Union[str, list[str]],
-    language: Union[str, list[str], None] = None,
-    ref_text: Union[str, list[str], None] = None,
-    ref_audio: Union[
-        str,
-        list[str],
-        tuple[torch.Tensor, int],
-        list[tuple[torch.Tensor, int]],
-        None,
-    ] = None,
-    voice_clone_prompt = None,
-    instruct: Union[str, list[str], None] = None,
-    duration: Union[float, list[Optional[float]], None] = None,
-    speed: Union[float, list[Optional[float]], None] = None,
-    generation_config=None,
-    **kwargs,
-) -> list:
-    if model.audio_tokenizer is None or model.text_tokenizer is None:
-        raise RuntimeError(
-            "Model is not loaded with audio/text tokenizers. Make sure you "
-            "loaded the model with OmniVoice.from_pretrained()."
-        )
-    
-    gen_config = (
-        generation_config
-        if generation_config is not None
-        else OmniVoiceGenerationConfig.from_dict(kwargs)
-    )
-
-    model.eval()
-
-    full_task = model._preprocess_all(
-        text=text,
-        language=language,
-        ref_text=ref_text,
-        ref_audio=ref_audio,
-        voice_clone_prompt=voice_clone_prompt,
-        instruct=instruct,
-        preprocess_prompt=gen_config.preprocess_prompt,
-        speed=speed,
-        duration=duration,
-    )
-
-    short_idx, long_idx = full_task.get_indices(
-        gen_config, model.audio_tokenizer.config.frame_rate
-    )
-
-    results = [None] * full_task.batch_size
-
-    if short_idx:
-        short_task = full_task.slice_task(short_idx)
-        short_results = model._generate_iterative(short_task, gen_config)
-        for idx, res in zip(short_idx, short_results):
-            results[idx] = res
-
-    if long_idx:
-        long_task = full_task.slice_task(long_idx)
-        long_results = model._generate_chunked(long_task, gen_config)
-        for idx, res in zip(long_idx, long_results):
-            results[idx] = res
-
-    generated_audios = []
-    for i in range(full_task.batch_size):
-        assert results[i] is not None, f"Result {i} was not generated"
-        generated_audios.append(
-            model._decode_and_post_process(
-                results[i], full_task.ref_rms[i], gen_config  # type: ignore[arg-type]
-            )
-        )
-
-    return generated_audios
-
 if __name__ == "__main__":
   # Load the model
   model = OmniVoice.from_pretrained(
@@ -109,7 +32,7 @@ if __name__ == "__main__":
   )
 
   # Generate audio
-  audio = generate(model,
+  audio = model.generate(
       text="Testing testing one two three, this is made with Omni-Voice. Can you hear me? or not? 谢谢你",
       ref_audio="voice.mp3",
       ref_text="Nothing is ever as it seems anymore and simple declarations bring deeper intrigue, which we are now going to have to spend today unpacking",
