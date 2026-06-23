e062fda6995cfbcfa9c9308e1df8814a37c7eec2
roryclear <roryclear.rc@gmail.com>
2026-06-23 14:33:48 +0100

    run test in omni.py

--- omni.py
+++ omni.py
@@ -199,6 +199,8 @@ def _resolve_model_path(name_or_path: str) -> str:
 
     return snapshot_download(name_or_path)
 
+class blank: pass
+
 HIDDEN_SIZE = 1024
 NUM_AUDIO_CODEBOOK = 8
 AUDIO_VOCAB_SIZE = 1025
@@ -567,4 +569,45 @@ _NONVERBAL_PATTERN = re.compile(
     r"question-ei|question-yi|surprise-ah|surprise-oh|surprise-wa|"
     r"surprise-yo|dissatisfaction-hnn)\]"
 )
- 
+
+
+import soundfile as sf
+import pickle
+
+if __name__ == "__main__":
+  model = OmniVoice.from_pretrained("k2-fsa/OmniVoice", device_map="mps:0", dtype=torch.float16)
+
+  torch.manual_seed(0)
+  audio = model.generate(
+      text="Testing testing one two three, this is made with Omni-Voice. Can you hear me? or not? 谢谢你",
+      ref_audio="voice.wav",
+      ref_text="Nothing is ever as it seems anymore and simple declarations bring deeper intrigue, which we are now going to have to spend today unpacking",
+  ) # audio is a list of `np.ndarray` with shape (T,) at 24 kHz.
+  #pickle.dump(audio, open("short.pkl", "wb"))
+  exp = pickle.load(open("short.pkl", "rb"))
+  sf.write("out.wav", audio, 24000)
+  np.testing.assert_allclose(exp, audio, rtol=1e-5)
+
+  '''
+  torch.manual_seed(0)
+  audio = model.generate(
+      text = "That's it, turn the page on the day, walk away 'Cause there's sense in what I say, I'm forty-fifth generation Roman But I don't know 'em or care when I'm spitting So return to your sitting position and listen, it's fitting That I'm miles ahead and they chase me Show your face on TV then we'll see, you can't do half My crew laughs at your rhubarb-and-custard verses You rain down curses, but I'm waving your hearses driving by Streets riding high with the beats in the sky All stare, eyes glazed, garage burnt down The fire raged for forty days and in forty ways But through the blaze, they see it fade The sea of black. That's it, turn the page on the day, walk away 'Cause there's sense in what I say, I'm forty-fifth generation Roman But I don't know 'em or care when I'm spitting So return to your sitting position and listen, it's fitting That I'm miles ahead and they chase me Show your face on TV then we'll see, you can't do half My crew laughs at your rhubarb-and-custard verses You rain down curses, but I'm waving your hearses driving by Streets riding high with the beats in the sky All stare, eyes glazed, garage burnt down The fire raged for forty days and in forty ways But through the blaze, they see it fade The sea of black",
+      ref_audio="voice.wav",
+      ref_text="Nothing is ever as it seems anymore and simple declarations bring deeper intrigue, which we are now going to have to spend today unpacking",
+  ) # audio is a list of `np.ndarray` with shape (T,) at 24 kHz.
+  #pickle.dump(audio, open("long.pkl", "wb"))
+  exp = pickle.load(open("long.pkl", "rb"))
+  sf.write("out_long.wav", audio, 24000)
+  np.testing.assert_allclose(exp, audio, rtol=1e-5)
+  '''
+
+  torch.manual_seed(0)
+  audio = model.generate(
+      text="Testing testing one two three, this is made with Omni-Voice. Can you hear me? or not? 谢谢你",
+      ref_audio="voice2.wav",
+      ref_text="And eh all of the people, I mean we have the greatest military anywhere in the world, and you saw that, in Iran, where, in one week virtually, we knocked out their entire navy, their entire air force",
+  ) # audio is a list of `np.ndarray` with shape (T,) at 24 kHz.
+  #pickle.dump(audio, open("short2.pkl", "wb"))
+  exp = pickle.load(open("short2.pkl", "rb"))
+  sf.write("out2.wav", audio, 24000)
+  np.testing.assert_allclose(exp, audio, rtol=1e-5)
