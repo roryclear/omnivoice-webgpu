2b7eecac247debd3a90069ff950fa601700e0ca3
roryclear <roryclear.rc@gmail.com>
2026-07-01 17:28:26 +0100

    clean a little

--- omni.py
+++ omni.py
@@ -741,7 +741,7 @@ class omni:
       scores = scores - (layer_ids * LAYER_PENTALTY_FACTOR)
       scores = _gumbel_sample(scores, POSITION_TEMP)
       scores = Tensor.where(tokens.reshape(NUM_AUDIO_CODEBOOK, target_length) == AUDIO_MASK_ID, scores, -float("inf"))
-      return pred_tokens.flatten(), scores.flatten()
+      return pred_tokens.flatten().cast(dtypes.long), scores.flatten()
 
   def generate(self, text=None, ref_text=None, ref_audio=None):
     ref_audio_tokens = self.create_voice_clone_prompt(ref_audio=ref_audio)
@@ -878,9 +878,9 @@ class omni:
         pred_tokens, scores = self(input_ids=batch_input_ids[:, :, 0:c_len], audio_mask=batch_audio_mask[:, 0:c_len]
                                    ,c_len=c_len, target_length=target_length, tokens=tokens.clone())
         
-        _, order = Tensor.sort(-scores)
+        _, order = Tensor.sort(scores, descending=True)
         idx = order[:sched[step]]
-        tokens[idx] = pred_tokens[idx].cast(tokens.dtype)
+        tokens[idx] = pred_tokens[idx]
 
         batch_input_ids[0: 1, :, c_len - target_length : c_len] = tokens.reshape(NUM_AUDIO_CODEBOOK, target_length)
         batch_input_ids[1:2, :, :target_length] = tokens.reshape(NUM_AUDIO_CODEBOOK, target_length)
