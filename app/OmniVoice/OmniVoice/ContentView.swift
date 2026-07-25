165af3905729340befcaf114af557de62d1247de
roryclear <roryclear.rc@gmail.com>
2026-07-25 16:21:51 +0100

    fix layer ids copyin

--- app/OmniVoice/OmniVoice/ContentView.swift
+++ app/OmniVoice/OmniVoice/ContentView.swift
@@ -471,8 +471,7 @@ func generateIterative(_ text: String, targetLength: Int, refText: String, refAu
         sched.append(num)
         rem -= num
     }
-    let layer_ids: [[Int32]] = (0..<NUM_AUDIO_CODEBOOK).map { [Int32($0)] }
-    //let layer_ids: [Int32] = Array(repeating: Int32(2), count: 8)
+    let layer_ids = (0..<NUM_AUDIO_CODEBOOK).map { Int32($0) }
     print("sched =", sched)
     // todo, here for now till the copyins are done
     model_graph = GraphRunner(filename: "1.rc")
@@ -483,9 +482,11 @@ func generateIterative(_ text: String, targetLength: Int, refText: String, refAu
     // audio_mask
     // tokens
     attentionMask.withUnsafeBytes { buffers[1134]!.contents().copyMemory(from: $0.baseAddress!, byteCount: $0.count) }
+    layer_ids.withUnsafeBytes { buffers[1136]!.contents().copyMemory(from: $0.baseAddress!, byteCount: $0.count)}
     // todo wrong
     //input_ids.withUnsafeBytes { buffers[1079]!.contents().copyMemory(from: $0.baseAddress!, byteCount: $0.count) }
     //audio_mask.withUnsafeBytes { buffers[1135]!.contents().copyMemory(from: $0.baseAddress!, byteCount: $0.count) }
+    //let layer_ids: [[Int32]] = (0..<NUM_AUDIO_CODEBOOK).map { [Int32($0)] }
     //layer_ids.withUnsafeBytes { buffers[1136]!.contents().copyMemory(from: $0.baseAddress!, byteCount: $0.count) }
     
     model_graph.run()
