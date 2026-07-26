bf4367a7d94b20561aaa96e5ca8bc577e46086e1
roryclear <roryclear.rc@gmail.com>
2026-07-26 22:38:52 +0100

    note pkl saves

--- omni.py
+++ omni.py
@@ -1014,7 +1014,7 @@ if __name__ == "__main__":
         ref_audio=open("voice4.wav", "rb").read(),
         ref_text="This is a wav file for my voice, so that omni voice can capture my voice. I need to talk for about 15 seconds emm we're on about eleven right now, so I just need to say a few more words, thank you"
     )
-    pickle.dump(audio, open("short4.pkl", "wb"))
+    #pickle.dump(audio, open("short4.pkl", "wb"))
     exp = pickle.load(open("short4.pkl", "rb"))
     write_waveform("out4.wav", audio)
     np.testing.assert_allclose(exp, audio, rtol=1e-5)
@@ -1025,7 +1025,7 @@ if __name__ == "__main__":
         ref_audio=open("voice.wav", "rb").read(),
         ref_text="Nothing is ever as it seems anymore and simple declarations bring deeper intrigue, which we are now going to have to spend today unpacking, as is likely clear, I am referring to the Iran deal",
     )
-    pickle.dump(audio, open("short.pkl", "wb"))
+    #pickle.dump(audio, open("short.pkl", "wb"))
     exp = pickle.load(open("short.pkl", "rb"))
     write_waveform("out.wav", audio)
     np.testing.assert_allclose(exp, audio, rtol=1e-5)
@@ -1036,7 +1036,7 @@ if __name__ == "__main__":
         ref_audio=open("voice2.wav", "rb").read(),
         ref_text="And eh all of the people, I mean we have the greatest military anywhere in the world, and you saw that, in Iran, where, in one week virtually, we knocked out their entire navy, their entire air force",
     ) # audio is a list of `np.ndarray` with shape (T,) at 24 kHz.
-    pickle.dump(audio, open("short1.pkl", "wb"))
+    #pickle.dump(audio, open("short1.pkl", "wb"))
     exp = pickle.load(open("short1.pkl", "rb"))
     write_waveform("out1.wav", audio)
     np.testing.assert_allclose(exp, audio, rtol=1e-5)
@@ -1049,7 +1049,7 @@ if __name__ == "__main__":
         ref_text="it's what non car people don't get, they see all cars as just, a tonne and a half, two tonnes of wires, glass metal and rubber, that's all they see. People like you or I know, we have an unshakeable belief that cars are living entities",
         num_steps=32
     ) # audio is a list of `np.ndarray` with shape (T,) at 24 kHz.
-    pickle.dump(audio, open("short2.pkl", "wb"))
+    #pickle.dump(audio, open("short2.pkl", "wb"))
     exp = pickle.load(open("short2.pkl", "rb"))
     write_waveform("out2.wav", audio)
     np.testing.assert_allclose(exp, audio, rtol=1e-5)
