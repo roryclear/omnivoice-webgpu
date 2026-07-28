05cf5f609979e42db0fa6616220dae200f07f00d
roryclear <roryclear.rc@gmail.com>
2026-07-28 14:06:14 +0100

    copyin attention mask

--- app/OmniVoice/OmniVoice/ContentView.swift
+++ app/OmniVoice/OmniVoice/ContentView.swift
@@ -496,6 +496,7 @@ func generateIterative(_ text: String, targetLength: Int, refText: String, refAu
     // input_ids = input_ids.map { $0.map { $0.map { $0 * 2 } } }
     input_ids.flatMap { $0 }.flatMap { $0 }.withUnsafeBytes { memcpy(buffers[1134]!.contents(), $0.baseAddress!, $0.count) }
     audio_mask.flatMap { $0 }.withUnsafeBytes { memcpy(buffers[1135]!.contents(), $0.baseAddress!, $0.count) }
+    attentionMask.flatMap { $0 }.flatMap { $0 }.flatMap { $0 }.withUnsafeBytes { memcpy(buffers[1136]!.contents(), $0.baseAddress!, $0.count) }
     
     model_graph.run(vals_dict: [547: c_len, 131: targetLength])
     
